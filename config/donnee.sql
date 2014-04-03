61d2966519f6f6829797e0458a5f9b912e9babfd
adriencolomer <adriencolomer@users.noreply.github.com>
2014-04-03 12:26:08 +0200

    ajout des différents remises
    ajout des différents remises

--- config/donnee.sql
+++ config/donnee.sql
@@ -1,4 +1,3 @@
-
 ;Insertion des adresses pour chacune des boutiques
 INSERT INTO Adresse(id_adresse ,code_postal,ville,rue,pays) VALUES (1,"95150","TAVERNY","4 place de la liberté","FRANCE");
 INSERT INTO Adresse(id_adresse ,code_postal,ville,rue,pays) VALUES (2,"75018","PARIS","Avenue FOCH","FRANCE");
@@ -10,3 +9,12 @@ INSERT INTO Boutique(numboutique,adresse) VALUES(1,1);
 INSERT INTO Boutique(numboutique,adresse) VALUES(2,2);
 INSERT INTO Boutique(numboutique,adresse) VALUES(3,3);
 INSERT INTO Boutique(numboutique,adresse) VALUES(4,4);
+
+;Insertion des causes
+INSERT INTO Cause(nom) VALUES ("périmé");
+INSERT INTO Cause(nom) VALUES ("erreur de livraison");
+
+;Insertion des remises
+INSERT INTO Remise(nom_remise,prix_max,prix_min,taux) VALUES ("Remise3%",229,381,3);
+INSERT INTO Remise(nom_remise,prix_max,prix_min,taux) VALUES ("Remise5%",381,1220,5);
+INSERT INTO Remise(nom_remise,prix_max,prix_min,taux) VALUES ("Remise5%",1220,9999.99,7);
